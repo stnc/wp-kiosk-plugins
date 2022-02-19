db7e23696bbda946a78afdf58ae88adb4b7db64b
selman tunç <selmantunc@hotmail.com>
2022-02-19 15:37:23 +0300

    ss

--- menuPages/ayarlar.php
+++ menuPages/ayarlar.php
@@ -1,4 +1,51 @@
 <?php
 function stncForm_adminMenu_About_contentsTest(){
-    echo "sdds";
+    ?>
+    <h1> <?php esc_html_e( 'Welcome to my custom admin page.', 'my-plugin-textdomain' ); ?> </h1>
+    <form method="POST" action="options.php">
+    <?php
+    settings_fields( 'sample-page' );
+    do_settings_sections( 'sample-page' );
+    submit_button();
+    ?>
+    </form>
+    <?php
+}
+
+
+
+
+add_action( 'admin_init', 'my_settings_init' );
+
+function my_settings_init() {
+
+    add_settings_section(
+        'sample_page_setting_section',
+        __( 'Custom settings', 'my-textdomain' ),
+        'my_setting_section_callback_function',
+        'sample-page'
+    );
+
+		add_settings_field(
+		   'my_setting_field',
+		   __( 'My custom setting field', 'my-textdomain' ),
+		   'my_setting_markup',
+		   'sample-page',
+		   'sample_page_setting_section'
+		);
+
+		register_setting( 'sample-page', 'my_setting_field' );
+}
+
+
+function my_setting_section_callback_function() {
+    echo '<p>Intro text for our settings section</p>';
+}
+
+
+function my_setting_markup() {
+    ?>
+    <label for="my-input"><?php _e( 'My Input' ); ?></label>
+    <input type="text" id="my_setting_field" name="my_setting_field" value="<?php echo get_option( 'my_setting_field' ); ?>">
+    <?php
 }
