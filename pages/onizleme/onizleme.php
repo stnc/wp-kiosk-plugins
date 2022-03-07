7e5a434f920363a4b7c6892dadbdb801179d6c61
Selman TUNÇ <selmantunc@yandex.com>
2022-03-07 18:04:15 +0300

    dfg

--- pages/onizleme/onizleme.php
+++ pages/onizleme/onizleme.php
@@ -15,6 +15,7 @@ function stnc_wp_kiosk_onizleme(){
     $root_categories = get_categories($list_child_terms_args);
       // print_r($root_categories[0] );
       // print_r($root_categories[0]->name );
+      //    <iframe src="/kiosk/?onizleme=1" name="iframe_a" style="width:100%;height:1980px" frameborder="0" allowfullscreen ></iframe>
     ?>
     <body>
         <div class="container-fluid ">
@@ -22,17 +23,24 @@ function stnc_wp_kiosk_onizleme(){
                 <div class="col-sm-12 col-lg-12">
                   <a target="_blank" style="color:red" href="/kiosk/?onizleme=1">Önizleme Sayfasını Yeni Sekmede Aç</a>
                 <div class="alert alert-danger" style="color:green" role="alert"> Lütfen görüntülenmesini istediğiniz binayı seçiniz</div>
-                   <?php foreach ($root_categories as $key => $row ):?>
+                <ul>
+                   <?php foreach ($root_categories as $key => $row ):?><li>
+                   
                     <a style="color:blue" target="iframe_a" href="/kiosk?onizleme=1&bina=<?php echo $row->term_id?>" class="btn btn-primary"><?php echo $row->name?></a>
-                <?php endforeach ?>
+                    </li>
+                    
+                    
+        <?php endforeach ?>
+                </ul>
                 </div>
             </div>
         </div>
+            
+     <iframe  name="iframe_a" style="width:100%;height:1980px" frameborder="0" allowfullscreen ></iframe>
         </body>
     
     </html>
      
-    
-     <iframe src="/kiosk/?onizleme=1" name="iframe_a" style="width:100%;height:1980px" frameborder="0" allowfullscreen ></iframe>
+
     <?php
     }
